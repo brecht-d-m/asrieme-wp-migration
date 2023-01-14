d8fa31b7ca6633343929ae94b9e3e25a2afffeb4
brecht_d_m <brecht_de_meulenaere@outlook.com>
2023-01-14 11:57:36 +0100

    veldgroepen en berichttypes - json en sql

---
+++ veldgroepen/update_veldgroepen.sql
@@ -0,0 +1,143 @@
+UPDATE wrx6_posts SET post_excerpt = 'lid_gender' WHERE post_excerpt = 'werkendLid_gender' AND post_name = 'field_63b20c87d37f0' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'lid_gender' WHERE meta_key = 'werkendLid_gender';
+UPDATE wrx6_postmeta SET meta_key = '_lid_gender' WHERE meta_key = '_werkendLid_gender' AND meta_value = 'field_63b20c87d37f0';
+
+UPDATE wrx6_posts SET post_excerpt = 'lid_mail' WHERE post_excerpt = 'werkendLid_email' AND post_name = 'field_62b2e258dab4c' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'lid_mail' WHERE meta_key = 'werkendLid_email';
+UPDATE wrx6_postmeta SET meta_key = '_lid_mail' WHERE meta_key = '_werkendLid_email' AND meta_value = 'field_62b2e258dab4c';
+
+UPDATE wrx6_posts SET post_excerpt = 'lid_fotoLink' WHERE post_excerpt = 'werkendLid_profielfotoLink' AND post_name = 'field_63b2094e51b83' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'lid_fotoLink' WHERE meta_key = 'werkendLid_profielfotoLink';
+UPDATE wrx6_postmeta SET meta_key = '_lid_fotoLink' WHERE meta_key = '_werkendLid_profielfotoLink' AND meta_value = 'field_63b2094e51b83';
+
+UPDATE wrx6_posts SET post_excerpt = 'lid_telefoon' WHERE post_excerpt = 'werkendLid_telefoon' AND post_name = 'field_63b00411bcd64' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'lid_telefoon' WHERE meta_key = 'werkendLid_telefoon';
+UPDATE wrx6_postmeta SET meta_key = '_lid_telefoon' WHERE meta_key = '_werkendLid_telefoon' AND meta_value = 'field_63b00411bcd64';
+
+UPDATE wrx6_posts SET post_excerpt = 'wedstrijdalbum_link' WHERE post_excerpt = 'wedstrijdfotos_url' AND post_name = 'field_63b3114deaa99' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'wedstrijdalbum_link' WHERE meta_key = 'wedstrijdfotos_url';
+UPDATE wrx6_postmeta SET meta_key = '_wedstrijdalbum_link' WHERE meta_key = '_wedstrijdfotos_url' AND meta_value = 'field_63b3114deaa99';
+
+UPDATE wrx6_posts SET post_excerpt = 'wedstrijdalbum_wedstrijd' WHERE post_excerpt = 'wedstrijdfotos_wedstrijd' AND post_name = 'field_63b31103eaa98' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'wedstrijdalbum_wedstrijd' WHERE meta_key = 'wedstrijdfotos_wedstrijd';
+UPDATE wrx6_postmeta SET meta_key = '_wedstrijdalbum_wedstrijd' WHERE meta_key = '_wedstrijdfotos_wedstrijd' AND meta_value = 'field_63b31103eaa98';
+
+UPDATE wrx6_posts SET post_excerpt = 'klassement_documenten' WHERE post_excerpt = 'klassement_docs' AND post_name = 'field_633c2c81177b1' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'klassement_documenten' WHERE meta_key = 'klassement_docs';
+UPDATE wrx6_postmeta SET meta_key = '_klassement_documenten' WHERE meta_key = '_klassement_docs' AND meta_value = 'field_633c2c81177b1';
+
+UPDATE wrx6_posts SET post_excerpt = 'wedstrijd_type' WHERE post_excerpt = 'match_type' AND post_name = 'field_5f4f61c8ba9ac' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'wedstrijd_type' WHERE meta_key = 'match_type';
+UPDATE wrx6_postmeta SET meta_key = '_wedstrijd_type' WHERE meta_key = '_match_type' AND meta_value = 'field_5f4f61c8ba9ac';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_subtitel' WHERE post_excerpt = 'eventAttr_subtitel' AND post_name = 'field_63b03e9e0002b' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_subtitel' WHERE meta_key = 'eventAttr_subtitel';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_subtitel' WHERE meta_key = '_eventAttr_subtitel' AND meta_value = 'field_63b03e9e0002b';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_tijd' WHERE post_excerpt = 'eventAttr_time' AND post_name = 'field_63b03ecf0002d' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_tijd' WHERE meta_key = 'eventAttr_time';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_tijd' WHERE meta_key = '_eventAttr_time' AND meta_value = 'field_63b03ecf0002d';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_titel' WHERE post_excerpt = 'eventAttr_title' AND post_name = 'field_63b03e7f0002a' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_titel' WHERE meta_key = 'eventAttr_title';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_titel' WHERE meta_key = '_eventAttr_title' AND meta_value = 'field_63b03e7f0002a';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_datum' WHERE post_excerpt = 'eventAttr_date' AND post_name = 'field_63b03eb10002c' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_datum' WHERE meta_key = 'eventAttr_date';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_datum' WHERE meta_key = '_eventAttr_date' AND meta_value = 'field_63b03eb10002c';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_heeftLocatieinfo' WHERE post_excerpt = 'eventAttr_heeftLocatieinfo' AND post_name = 'field_63b00c5becfe8' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_heeftLocatieinfo' WHERE meta_key = 'eventAttr_heeftLocatieinfo';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_heeftLocatieinfo' WHERE meta_key = '_eventAttr_heeftLocatieinfo' AND meta_value = 'field_63b00c5becfe8';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_adres' WHERE post_excerpt = 'eventAttr_adres' AND post_name = 'field_63b00c94ecfe9' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_adres' WHERE meta_key = 'eventAttr_adres';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_adres' WHERE meta_key = '_eventAttr_adres' AND meta_value = 'field_63b00c94ecfe9';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_contactpersoon' WHERE post_excerpt = 'eventAttr_contactpersoon' AND post_name = 'field_63b28f5821d42' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_contactpersoon' WHERE meta_key = 'eventAttr_contactpersoon';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_contactpersoon' WHERE meta_key = '_eventAttr_contactpersoon' AND meta_value = 'field_63b28f5821d42';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_heeftContactpersoon' WHERE post_excerpt = 'eventAttr_heeftContactpersoon' AND post_name = 'field_63b00d2aecfeb' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_heeftContactpersoon' WHERE meta_key = 'eventAttr_heeftContactpersoon';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_heeftContactpersoon' WHERE meta_key = '_eventAttr_heeftContactpersoon' AND meta_value = 'field_63b00d2aecfeb';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_inschrijvingslink' WHERE post_excerpt = 'eventAttr_inschrijvingslink' AND post_name = 'field_63b00bc5ecfe4' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_inschrijvingslink' WHERE meta_key = 'eventAttr_inschrijvingslink';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_inschrijvingslink' WHERE meta_key = '_eventAttr_inschrijvingslink' AND meta_value = 'field_63b00bc5ecfe4';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_uitersteInschrijfdatum' WHERE post_excerpt = 'eventAttr_uitersteInschrijfdatum' AND post_name = 'field_63b00e4decfef' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_uitersteInschrijfdatum' WHERE meta_key = 'eventAttr_uitersteInschrijfdatum';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_uitersteInschrijfdatum' WHERE meta_key = '_eventAttr_uitersteInschrijfdatum' AND meta_value = 'field_63b00e4decfef';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_heeftUitersteInschrijfdatum' WHERE post_excerpt = 'eventAttr_heeftUitersteInschrijfdatum' AND post_name = 'field_63b00e12ecfee' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_heeftUitersteInschrijfdatum' WHERE meta_key = 'eventAttr_heeftUitersteInschrijfdatum';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_heeftUitersteInschrijfdatum' WHERE meta_key = '_eventAttr_heeftUitersteInschrijfdatum' AND meta_value = 'field_63b00e12ecfee';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_heeftVerplichtInschrijven' WHERE post_excerpt = 'eventAttr_heeftVerplichtInschrijven' AND post_name = 'field_63b00bdeecfe5' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_heeftVerplichtInschrijven' WHERE meta_key = 'eventAttr_heeftVerplichtInschrijven';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_heeftVerplichtInschrijven' WHERE meta_key = '_eventAttr_heeftVerplichtInschrijven' AND meta_value = 'field_63b00bdeecfe5';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_extraInfotekst' WHERE post_excerpt = 'eventAttr_extraInfotekst' AND post_name = 'field_63b00c2cecfe6' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_extraInfotekst' WHERE meta_key = 'eventAttr_extraInfotekst';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_extraInfotekst' WHERE meta_key = '_eventAttr_extraInfotekst' AND meta_value = 'field_63b00c2cecfe6';
+
+UPDATE wrx6_posts SET post_excerpt = 'activiteit_heeftInschrijvingsinfo' WHERE post_excerpt = 'eventAttr_heeftInschrijvingsinfo' AND post_name = 'field_63b00ba3ecfe3' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'activiteit_heeftInschrijvingsinfo' WHERE meta_key = 'eventAttr_heeftInschrijvingsinfo';
+UPDATE wrx6_postmeta SET meta_key = '_activiteit_heeftInschrijvingsinfo' WHERE meta_key = '_eventAttr_heeftInschrijvingsinfo' AND meta_value = 'field_63b00ba3ecfe3';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainingsgroep_coordinator' WHERE post_excerpt = 'traininggroup_coordinator' AND post_name = 'field_628cbd86f586c' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainingsgroep_coordinator' WHERE meta_key = 'traininggroup_coordinator';
+UPDATE wrx6_postmeta SET meta_key = '_trainingsgroep_coordinator' WHERE meta_key = '_traininggroup_coordinator' AND meta_value = 'field_628cbd86f586c';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainingsgroep_info' WHERE post_excerpt = 'traininggroup_info' AND post_name = 'field_633c35375e417' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainingsgroep_info' WHERE meta_key = 'traininggroup_info';
+UPDATE wrx6_postmeta SET meta_key = '_trainingsgroep_info' WHERE meta_key = '_traininggroup_info' AND meta_value = 'field_633c35375e417';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainingsopdracht_seqNo' WHERE post_excerpt = 'trainingassignment_seqNo' AND post_name = 'field_63088c0569294' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainingsopdracht_seqNo' WHERE meta_key = 'trainingassignment_seqNo';
+UPDATE wrx6_postmeta SET meta_key = '_trainingsopdracht_seqNo' WHERE meta_key = '_trainingassignment_seqNo' AND meta_value = 'field_63088c0569294';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainingsopdracht_trainer' WHERE post_excerpt = 'trainerassignment_trainer' AND post_name = 'field_628cdd5aceb7b' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainingsopdracht_trainer' WHERE meta_key = 'trainerassignment_trainer';
+UPDATE wrx6_postmeta SET meta_key = '_trainingsopdracht_trainer' WHERE meta_key = '_trainerassignment_trainer' AND meta_value = 'field_628cdd5aceb7b';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainingsopdracht_specialisatie' WHERE post_excerpt = 'trainingassignment_specialisatie' AND post_name = 'field_628cdd2cb844d' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainingsopdracht_specialisatie' WHERE meta_key = 'trainingassignment_specialisatie';
+UPDATE wrx6_postmeta SET meta_key = '_trainingsopdracht_specialisatie' WHERE meta_key = '_trainingassignment_specialisatie' AND meta_value = 'field_628cdd2cb844d';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainingsopdracht_trainingsgroep' WHERE post_excerpt = 'trainingassignment_traininggroup' AND post_name = 'field_628cdd8cceb7c' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainingsopdracht_trainingsgroep' WHERE meta_key = 'trainingassignment_traininggroup';
+UPDATE wrx6_postmeta SET meta_key = '_trainingsopdracht_trainingsgroep' WHERE meta_key = '_trainingassignment_traininggroup' AND meta_value = 'field_628cdd8cceb7c';
+
+UPDATE wrx6_posts SET post_excerpt = 'werkgroep_mail' WHERE post_excerpt = 'werkgroep_mailAddress' AND post_name = 'field_634e638bba4c3' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'werkgroep_mail' WHERE meta_key = 'werkgroep_mailAddress';
+UPDATE wrx6_postmeta SET meta_key = '_werkgroep_mail' WHERE meta_key = '_werkgroep_mailAddress' AND meta_value = 'field_634e638bba4c3';
+
+UPDATE wrx6_posts SET post_excerpt = 'pagina_contact' WHERE post_excerpt = 'page_contact' AND post_name = 'field_63b2835587a87' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'pagina_contact' WHERE meta_key = 'page_contact';
+UPDATE wrx6_postmeta SET meta_key = '_pagina_contact' WHERE meta_key = '_page_contact' AND meta_value = 'field_63b2835587a87';
+
+UPDATE wrx6_posts SET post_excerpt = 'pagina_hoofdTitel' WHERE post_excerpt = 'page_mainTitle' AND post_name = 'field_633c135048dc6' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'pagina_hoofdTitel' WHERE meta_key = 'page_mainTitle';
+UPDATE wrx6_postmeta SET meta_key = '_pagina_hoofdTitel' WHERE meta_key = '_page_mainTitle' AND meta_value = 'field_633c135048dc6';
+
+UPDATE wrx6_posts SET post_excerpt = 'pagina_contactInfo' WHERE post_excerpt = 'page_contact_info' AND post_name = 'field_63aea4733423b' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'pagina_contactInfo' WHERE meta_key = 'page_contact_info';
+UPDATE wrx6_postmeta SET meta_key = '_pagina_contactInfo' WHERE meta_key = '_page_contact_info' AND meta_value = 'field_63aea4733423b';
+
+UPDATE wrx6_posts SET post_excerpt = 'pagina_slogan' WHERE post_excerpt = 'page_slogan' AND post_name = 'field_633c05972ac11' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'pagina_slogan' WHERE meta_key = 'page_slogan';
+UPDATE wrx6_postmeta SET meta_key = '_pagina_slogan' WHERE meta_key = '_page_slogan' AND meta_value = 'field_633c05972ac11';
+
+UPDATE wrx6_posts SET post_excerpt = 'pagina_heeftContact' WHERE post_excerpt = 'page_contactPersoon' AND post_name = 'field_63aea40f34238' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'pagina_heeftContact' WHERE meta_key = 'page_contactPersoon';
+UPDATE wrx6_postmeta SET meta_key = '_pagina_heeftContact' WHERE meta_key = '_page_contactPersoon' AND meta_value = 'field_63aea40f34238';
+
+UPDATE wrx6_posts SET post_excerpt = 'wedstrijduitslag_link' WHERE post_excerpt = 'wedstrijduitslag_url' AND post_name = 'field_61772dc918565' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'wedstrijduitslag_link' WHERE meta_key = 'wedstrijduitslag_url';
+UPDATE wrx6_postmeta SET meta_key = '_wedstrijduitslag_link' WHERE meta_key = '_wedstrijduitslag_url' AND meta_value = 'field_61772dc918565';
+
+UPDATE wrx6_posts SET post_excerpt = 'trainer_fotoLink' WHERE post_excerpt = 'trainer_fotoUrl' AND post_name = 'field_63b1c9ae1c2a4' AND post_type = 'acf-field';
+UPDATE wrx6_postmeta SET meta_key = 'trainer_fotoLink' WHERE meta_key = 'trainer_fotoUrl';
+UPDATE wrx6_postmeta SET meta_key = '_trainer_fotoLink' WHERE meta_key = '_trainer_fotoUrl' AND meta_value = 'field_63b1c9ae1c2a4';
